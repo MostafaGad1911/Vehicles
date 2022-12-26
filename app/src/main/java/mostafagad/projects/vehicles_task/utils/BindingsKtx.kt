7e15d0eb37a6bab5ca4eb30306b05840a1f0de5a
MostafaGad1911 <mostafa.gad@kashat.com.eg>
2022-12-26 13:14:53 +0200

    Handle invalid colors .

--- app/src/main/java/mostafagad/projects/vehicles_task/utils/BindingsKtx.kt
+++ app/src/main/java/mostafagad/projects/vehicles_task/utils/BindingsKtx.kt
@@ -1,21 +1,17 @@
 package mostafagad.projects.vehicles_task.utils
 
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import mostafagad.projects.vehicles_task.R
-import javax.sql.DataSource
 
 object BindingsKtx{
     @JvmStatic
@@ -62,7 +58,11 @@ object BindingsKtx{
     fun textColor(vehicleTxt: TextView, color: String?) {
         (color != null).let {
             if (it) {
-                vehicleTxt.setTextColor(Color.parseColor(color))
+                try {
+                    vehicleTxt.setTextColor(Color.parseColor(color))
+                }catch (e:Exception){
+                    vehicleTxt.setTextColor(Color.parseColor("#000000"))
+                }
             }
         }
     }
